00576c975f3f3210d3fb20ff30e26148288c8456
daniel <danielsoledad@gmail.com>
2020-12-13 23:16:27 -0800

    adding server.js file and trying to see If im able to launch team.html to heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+//Dependencies
+const express = require("express")
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.get("/", function(res, req){
+    res.sendFile(path.join(__dirname, "team.html"));
+})
+
+//listeners
+app.listen(PORT, function() {
+    console.log("App listening on http://localhost:" + PORT);
+  });
